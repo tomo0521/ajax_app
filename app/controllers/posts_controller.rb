319f1e8ebaf4cdbdaf08600797785c69af8951a1
tomonori iwasaki <tom.worker44@gmail.com>
2023-04-07 18:23:10 +0900

    firstcommit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.order(id: "DESC")
+  end
+
+  def create
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+  end
+
+end
